56d2788c0589a06857cb6dd87c827906e6b01b54
Guido Kestel <guido.gk@gmail.com>
2023-06-27 20:58:02 -0300

    Added a more specific selector
    added a more specific selector for the button "submit"

--- cypress/e2e/LoginTests.cy.js
+++ cypress/e2e/LoginTests.cy.js
@@ -7,8 +7,8 @@ describe('Login Tests', () => {
   it('Sign in with empty user and password should give me an error message', () => {
     cy.get('#username').should('be.empty')
     cy.get('#password').should('be.empty')
-    cy.get('[type="submit"]').should('be.visible')
-    cy.get('[type="submit"]').click()
+    cy.get('button[type="submit"]').should('be.visible')
+    cy.get('button[type="submit"]').click()
     cy.get('#flash').should('contain','Your username is invalid')
     cy.get('#flash').invoke('css', 'background-color').should('equal', 'rgb(198, 15, 19)')
   })
@@ -16,8 +16,8 @@ describe('Login Tests', () => {
   it('Sign in with not registered user and password should give me an error message', () => {
     cy.get('#username').type('wrongid')
     cy.get('#password').type('wrongpassword')
-    cy.get('[type="submit"]').should('be.visible')
-    cy.get('[type="submit"]').click()
+    cy.get('button[type="submit"]').should('be.visible')
+    cy.get('button[type="submit"]').click()
     cy.get('#flash').should('contain','Your username is invalid')
     cy.get('#flash').invoke('css', 'background-color').should('equal', 'rgb(198, 15, 19)')
   })
@@ -25,8 +25,8 @@ describe('Login Tests', () => {
   it('Sign in with a registered user and password should redirect me to a secure area', () => {
     cy.get('#username').type('tomsmith')
     cy.get('#password').type('SuperSecretPassword!')
-    cy.get('[type="submit"]').should('be.visible')
-    cy.get('[type="submit"]').click()
+    cy.get('button[type="submit"]').should('be.visible')
+    cy.get('button[type="submit"]').click()
     cy.url().should('eq','https://the-internet.herokuapp.com/secure')
 
     //Some extra checks in secure page. Note: I assume that there is a specific test file to test the '/secure' page
@@ -41,8 +41,8 @@ describe('Login Tests', () => {
   it('Sign in with registered user but invalid password should give me an error message', () => {
     cy.get('#username').type('tomsmith')
     cy.get('#password').type('wrongpassword')
-    cy.get('[type="submit"]').should('be.visible')
-    cy.get('[type="submit"]').click()
+    cy.get('button[type="submit"]').should('be.visible')
+    cy.get('button[type="submit"]').click()
     cy.get('#flash').should('contain','Your password is invalid!') // Please note that giving the information that the username is valid might be a security breach
     cy.get('#flash').invoke('css', 'background-color').should('equal', 'rgb(198, 15, 19)')
   })
